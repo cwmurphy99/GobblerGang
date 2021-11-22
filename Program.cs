3955b551433b924f5bae4213e1ba26bcce86ed37
Chris Murphy <theworldisyours@gmail.com>
2021-11-22 11:59:38 -0500

    phase 2

--- Program.cs
+++ Program.cs
@@ -8,18 +8,40 @@ namespace TheGobblerGang
         {
             Console.WriteLine("Plan your heist!");
             Console.WriteLine("----------------");
-            Console.WriteLine("Enter a team member name> ");
-            string Name = Console.ReadLine();
-            Console.WriteLine("Enter a skill level> ");
-            int SkillLevel = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Enter a Courage Factor> ");
-            float Courage = float.Parse(Console.ReadLine());
+            MemberPrompt();
 
-            Console.WriteLine("----------------------");
-            Console.WriteLine("Member Name: " + Name);
-            Console.WriteLine("Skill: " + SkillLevel);
-            Console.WriteLine("Courage: " + Courage);
+
         }
 
+        static void MemberPrompt()
+        {
+
+            Console.WriteLine("Enter a team member name> ");
+            string Name = Console.ReadLine();
+            if (Name == "")
+            {
+                return;
+            }
+            else
+            {
+
+                Console.WriteLine("Enter a skill level> ");
+                int SkillLevel = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Enter a Courage Factor> ");
+                float Courage = float.Parse(Console.ReadLine());
+
+                TeamMember NewMember = new TeamMember()
+                {
+                    Name = Name,
+                    Skill = SkillLevel,
+                    Courage = Courage
+                };
+                Console.WriteLine("----------------------");
+                Console.WriteLine(NewMember.TeamMemberInfo);
+                MemberPrompt();
+                Console.WriteLine("member total: ", NewMember.TotalMemberNumber)
+            }
+
+        }
     }
 }
--- TeamMember.cs
+++ TeamMember.cs
@@ -5,9 +5,25 @@ namespace TheGobblerGang
 
     public class TeamMember
     {
+        private static int _memberIncrementer = 0;
+
+        public TeamMember()
+        {
+            TotalMemberNumber = _memberIncrementer++;
+        }
         public string Name { get; set; }
         public int Skill { get; set; }
         public float Courage { get; set; }
+        public int TotalMemberNumber { get; set; }
 
+        public string TeamMemberInfo
+        {
+            get
+            {
+                return $"Name: {Name}\nSkill Level: {Skill}\nCourage Factor: {Courage}";
+            }
+        }
     }
+
+
 }
