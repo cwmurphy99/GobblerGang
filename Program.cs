a5e27c80177b185a95dbd85bb14b174299653ecd
Chris Murphy <theworldisyours@gmail.com>
2021-11-22 16:35:50 -0500

    phase 5

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-
 namespace TheGobblerGang
 {
     class Program
     {
+        public static List<TeamMember> Team = new List<TeamMember>();
+        public static int luckValue = new Random().Next(-10, 10);
+        public static int bankDifficulty = 100 + luckValue;
+        public static int totalSkill = 0;
         static void Main(string[] args)
         {
+            Console.Clear();
             Console.WriteLine("Plan your heist!");
             Console.WriteLine("----------------");
             MemberPrompt();
@@ -14,14 +18,38 @@ namespace TheGobblerGang
             {
                 MemberPrompt();
             }
-
+            foreach (TeamMember teamate in Team)
+            {
+                Console.WriteLine(teamate.TeamMemberInfo);
+                totalSkill += teamate.Skill;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Number of Members: {Team[Team.Count - 1].TotalMemberNumber}");
+            Console.ReadLine();
+            Console.Clear();
+            if (totalSkill >= bankDifficulty)
+            {
+                Console.WriteLine("");
+                Console.WriteLine($"Total Skill Level: {totalSkill}");
+                Console.WriteLine();
+                Console.WriteLine($"Bank Difficulty: {bankDifficulty}");
+                Console.WriteLine();
+                Console.WriteLine("Nice Job!");
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine($"Total Skill Level: {totalSkill}");
+                Console.WriteLine();
+                Console.WriteLine($"Bank Difficulty: {bankDifficulty}");
+                Console.WriteLine();
+                Console.WriteLine("You Suck.");
+            }
         }
-
         static bool MemberPrompt()
         {
             Console.Write("\nMember name> ");
             string Name = Console.ReadLine();
-            List<TeamMember> Team = new List<TeamMember>();
             if (Name == "")
             {
                 return false;
@@ -32,7 +60,6 @@ namespace TheGobblerGang
                 int SkillLevel = Int32.Parse(Console.ReadLine());
                 Console.Write("Courage Factor> ");
                 float Courage = float.Parse(Console.ReadLine());
-
                 TeamMember NewMember = new TeamMember()
                 {
                     Name = Name,
@@ -40,7 +67,6 @@ namespace TheGobblerGang
                     Courage = Courage
                 };
                 Team.Add(NewMember);
-                Console.WriteLine("\n----------------------");
                 return true;
             }
         }
