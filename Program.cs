482197687793d57dd347e412c268e02afadb4ed0
Chris Murphy <theworldisyours@gmail.com>
2021-11-22 11:30:31 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TheGobblerGang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plan your heist!");
+            Console.WriteLine("----------------");
+            Console.WriteLine("Enter a team member name> ");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Enter a skill level> ");
+            int SkillLevel = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a Courage Factor> ");
+            float Courage = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Member Name: " + Name);
+            Console.WriteLine("Skill: " + SkillLevel);
+            Console.WriteLine("Courage: " + Courage);
+        }
+
+    }
+}
---
+++ TeamMember.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace TheGobblerGang
+{
+
+    public class TeamMember
+    {
+        public string Name { get; set; }
+        public int Skill { get; set; }
+        public float Courage { get; set; }
+
+    }
+}
